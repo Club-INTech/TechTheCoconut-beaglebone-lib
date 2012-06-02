1083c7b9b5946c9da3814ae20331fa7a5c2710b7
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-06-02 15:39:08 +0200

    MutexFile : Avancées et nouvelles fonctions
    	ø Résolution du bug de placement dans le fichier.
    	+ Ajout d'une fonctionnalité permettant de vider le fichier
      	   lors de sa première ouverture par le programme.
    	+ Ajout d'une fonction de fermeture du fichier.
    	+ Ajout d'un pattern pour l'écriture de messages ("\n")
    	* Pour l'instant, aucune gestion si le mutex est à True en écriture

--- Code/file_mutex.py
+++ Code/file_mutex.py
@@ -12,10 +12,14 @@ class MutexFile :
     def __str__(self) :
         return str(MutexFile.mutex)
         
+    # Retourne True si le mutex pour ce fichier existait déjà, False sinon
     def addMutex(self, nom) :
         # Ajout du mutex s'il n'existe pas déjà.
         if not nom in MutexFile.mutex :
             MutexFile.mutex.update({nom:False})
+            return False
+        return True
+            
         
     def getMutex(self, nom) :
         return MutexFile.mutex[nom]
@@ -24,16 +28,24 @@ class MutexFile :
         MutexFile.mutex[nom] = etat
     
 class File :
-    def __init__(self, nom) :
+    # L'argument deleteAll détermine si oui ou non on veut supprimer les données
+    # contenues dans le fichier avant utilisation. (ne peut se faire que pour la
+    # première instance de ce fichier).
+    def __init__(self, nom, deleteAll = True) :
         if not hasattr(File, "mutex") :
             File.mutex = MutexFile()
+            File.pattern = "\n"
         
         self.nom = nom
         
         # Ouverture du fichier
         try :
-            self.fichier = open(self.nom, "w")
-            File.mutex.addMutex(self.nom)
+            dejaOuvert = File.mutex.addMutex(self.nom)
+            if (not dejaOuvert) and deleteAll: 
+                self.fichier = open(self.nom, "w")
+            else :
+                self.fichier = open(self.nom, "a")
+            
         except :
             print "Impossible d'ouvrir le fichier " + nom + "."
             
@@ -42,8 +54,17 @@ class File :
         # On checke le mutex
         if not File.mutex.getMutex(self.nom) :
             print "Ecriture du message..."
+            # On met le mutex à True
             File.mutex.setMutex(self.nom, True)
-            self.fichier.write(message)
+            # On va à la fin du fichier
+            self.fichier.seek(0,2)
+            # Pour écrire le message
+            self.fichier.write(str(message) + File.pattern)
+            # On force l'écriture
             self.fichier.flush()
+            # On remet le mutex à False
             File.mutex.setMutex(self.nom, False)
+            
+    def close(self) :
+        self.fichier.close()
         
